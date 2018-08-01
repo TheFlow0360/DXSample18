a5c9b3eab74f531a960effcee8903aca236713e4
Florian Koch <floriankochde@arcor.de>
2018-08-01 16:28:08 +0200

    modified sample

---
+++ dxSample/ViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.Mvvm;
+using DevExpress.Mvvm.DataAnnotations;
+
+namespace dxSample
+{
+    public class ViewModel: ViewModelBase
+    {
+        [Command]
+        public void OpenWorkspace() {
+            System.Diagnostics.Debug.WriteLine("Test");
+        }
+    }
+}
